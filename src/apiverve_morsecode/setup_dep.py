c199208eedde4b28044cbeff1f1e6b5cc70e78a1
APIVerve Team <hello@apiverve.com>
2025-03-28 17:35:35 -0500

    Bug Fixes

---
+++ src/apiverve_morsecode/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_morsecode',
+    version='1.1.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Morse Code is a simple tool for encoding and decoding Morse code. It returns the encoded or decoded Morse code.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
